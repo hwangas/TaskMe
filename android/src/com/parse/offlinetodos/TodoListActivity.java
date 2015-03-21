68392f41c33ba961e6a4418d240a1576840cf90a
hwangas <ahwang25@gmail.com>
2015-03-21 12:25:13 -0500

    for some reason the import of the actionmenuview was breaking it

--- android/src/com/parse/offlinetodos/TodoListActivity.java
+++ android/src/com/parse/offlinetodos/TodoListActivity.java
@@ -15,7 +15,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ActionMenuView;
+//import android.widget.ActionMenuView;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.LinearLayout;
